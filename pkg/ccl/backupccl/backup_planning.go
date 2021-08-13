26b36aba32a65010c77017607705cbe22f1d0993
David Taylor <tinystatemachine@gmail.com>
2021-08-13 14:28:50 +0000

    backupccl: add SHOW BACKUP WITH debug_dump_metadata_sst
    This is just a debugging tool for inspecting a new backup metadata SST.
    
    Release note: none.

--- pkg/ccl/backupccl/backup_planning.go
+++ pkg/ccl/backupccl/backup_planning.go
@@ -57,15 +57,16 @@ import (
 )
 
 const (
-	backupOptRevisionHistory = "revision_history"
-	backupOptEncPassphrase   = "encryption_passphrase"
-	backupOptEncKMS          = "kms"
-	backupOptWithPrivileges  = "privileges"
-	backupOptAsJSON          = "as_json"
-	backupOptWithDebugIDs    = "debug_ids"
-	backupOptIncStorage      = "incremental_storage"
-	localityURLParam         = "COCKROACH_LOCALITY"
-	defaultLocalityValue     = "default"
+	backupOptRevisionHistory  = "revision_history"
+	backupOptEncPassphrase    = "encryption_passphrase"
+	backupOptEncKMS           = "kms"
+	backupOptWithPrivileges   = "privileges"
+	backupOptAsJSON           = "as_json"
+	backupOptWithDebugIDs     = "debug_ids"
+	backupOptIncStorage       = "incremental_storage"
+	localityURLParam          = "COCKROACH_LOCALITY"
+	defaultLocalityValue      = "default"
+	backupOptDebugMetadataSST = "debug_dump_metadata_sst"
 )
 
 type tableAndIndex struct {
